5fe2a275a4e2c64bd3dcc2aace495bb64d1e9a76
David Rosales <mathiasrosales.25@gmail.com>
2025-11-26 07:19:39 -0500

    chore: initialize express project with git

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// src/index.js
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('API running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
